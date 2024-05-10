0b8e2c783fb76fc840a7cf38d67119d46ef67c19
+ <+>
2024-05-10 16:46:54 +0300

    updated

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,34 +2,30 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="container py-5">
+    <div id='aboutcompo'>
+    <div className="container py-5" >
       <div className="row">
-        <div className="col-lg-8 mx-auto">
-          <h2 className="mb-4">About Us</h2>
-          <p>Welcome to our e-commerce store!</p>
-          <p>We are dedicated to providing high-quality products and excellent services to our customers.</p>
-          <p>Our mission is to make your shopping experience enjoyable and hassle-free.</p>
-          <p>Feel free to browse through our collection of products and don't hesitate to contact us if you have any questions.</p>
-          <p>Thank you for choosing us!</p>
-
+        <div className="col-lg-8 mx-auto" >
+          <div id='aboutus'>
+            <h2 className="mb-4">About Us</h2>
+          <p>Welcome to our e-commerce store!
+          We are dedicated to providing high-quality products and excellent services to our customers.
+          Our mission is to make your shopping experience enjoyable and hassle-free.
+          Feel free to browse through our collection of products and don't hesitate to contact us if you have any questions.
+          Thank you for choosing us!</p>
+          </div>
+          <div>
           <h2 className="mt-5 mb-4">Contact Us</h2>
           <p>If you have any questions or concerns, please don't hesitate to get in touch with us:</p>
           <ul>
-            <li>Email: Ekart@company.com</li>
-            <li>Phone: 123-456-7890</li>
-            <li>Address: 123 Main Street, Nairobi, Kenya</li>
+            <li>Email: <a href='#' style={{textDecoration:'none'}}>Ekart@company.com</a></li>
+            <li>Phone: <a href='#'style={{textDecoration:'none'}}>123-456-7890</a></li>
+            <li>Address: <a href='#'style={{textDecoration:'none'}}>123 Main Street, Nairobi, Kenya</a></li>
           </ul>
-
-          <h2 className="mt-5 mb-4">Why Shop with us?</h2>
-          <ul>
-            <li>Secure checkout process</li>
-            <li>Fast and reliable shipping</li>
-            <li>Easy returns and exchanges</li>
-            <li>Customer reviews and ratings</li>
-            <li>Responsive customer support</li>
-          </ul>
+          </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
--- src/component/CustomerList.jsx
+++ src/component/CustomerList.jsx
@@ -1,89 +1,54 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class CustomersList extends Component {
-  //Using state prop to make content/value to be rendered dynamic/ up-to-date;
-  state = {
-    mainTitle: "Customers",
-    customersCount: 10,
-    customers: [
-      {
-        id: 1,
-        name: "Little Princess",
-        contacts: "123-456",
-        address: { city: "HeavenHold" },
-        image: "https://picsum.photos/id/7/60",
-      },
-      {
-        id: 2,
-        name: "Lorraine",
-        contacts: null,
-        address: { city: "Nairobi" },
-        image: "https://picsum.photos/id/999/60",
-      },
-      {
-        id: 3,
-        name: "Marrianne",
-        contacts: "111-333",
-        address: { city: "Lake-Side" },
-        image: "https://picsum.photos/id/1014/60",
-      },
-      {
-        id: 4,
-        name: "Sohee",
-        contacts: "987-654",
-        address: { city: "AstroWorld" },
-        image: "https://picsum.photos/id/203/60",
-      },
-      {
-        id: 5,
-        name: "Plague Doctor",
-        contacts: null,
-        address: { city: "England-Bro" },
-        image: "https://picsum.photos/id/900/60",
-      },
-    ],
+const CustomersList = () => {
+  // Using state hook to make content/value to be rendered dynamic/up-to-date
+  const [customersCount, setCustomersCount] = useState(10);
+  const [customers, setCustomers] = useState([
+    {
+      id: 1,
+      name: "Little Princess",
+      contacts: "123-456",
+      address: { city: "HeavenHold" },
+      image: "https://picsum.photos/id/7/60",
+    },
+    {
+      id: 2,
+      name: "Lorraine",
+      contacts: null,
+      address: { city: "Nairobi" },
+      image: "https://picsum.photos/id/999/60",
+    },
+    {
+      id: 3,
+      name: "Marrianne",
+      contacts: "111-333",
+      address: { city: "Lake-Side" },
+      image: "https://picsum.photos/id/1014/60",
+    },
+    {
+      id: 4,
+      name: "Sohee",
+      contacts: "987-654",
+      address: { city: "AstroWorld" },
+      image: "https://picsum.photos/id/203/60",
+    },
+    {
+      id: 5,
+      name: "Plague Doctor",
+      contacts: null,
+      address: { city: "England-Bro" },
+      image: "https://picsum.photos/id/900/60",
+    },
+  ]);
+
+  // Executes when the user clicks on the Reload button
+  const onReloadClick = () => {
+    // Update the state for the react to automatically update the Browser DOM
+    setCustomersCount(1000);
   };
 
-  render() {
-    return (
-      <>
-        <h4 className="m-2 p-2">
-          {this.state.mainTitle}
-
-          <span className="badge text-bg-secondary m-2">
-            {this.state.customersCount}
-          </span>
-
-          <button className="btn btn-info" onClick={this.onReloadClick}>
-            Reload
-          </button>
-        </h4>
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Picture</th>
-              <th>Customer Name</th>
-              <th>Contact Info</th>
-              <th>City</th>
-            </tr>
-          </thead>
-          <tbody>{this.getCustomerRow()}</tbody>
-        </table>
-      </>
-    );
-  }
-
-  //Executes when the user clicks on the Reload button
-  onReloadClick = () => {
-    //UPdate the state (setState()) for the react to automatically update the Browser DOM;
-    // console.log("Reload button clicked")
-    this.setState({ customersCount: 1000 });
-  };
-
-  //Adding a render method as an arrow function;
-  getContactsToRender = (contacts) => {
+  // Adding a function to get contacts to render
+  const getContactsToRender = (contacts) => {
     if (contacts) return contacts;
     else {
       return (
@@ -91,53 +56,61 @@ class CustomersList extends Component {
       );
     }
   };
+  // Function to handle changing the picture
+  const onChangePictureClick = (index) => {
+    let updatedCustomers = [...customers];
+    updatedCustomers[index].image = "https://picsum.photos/id/77/60";
+    setCustomers(updatedCustomers);
+  };
 
-  getCustomerRow = () => {
-    return (
-      //Using .map() to display and iterate over each item in table array;
-      this.state.customers.map((c, index) => {
-        return (
-          <tr key={c.id}>
-            <td>{c.id}</td>
-            <td>
-              <img src={c.image} alt="Mug-Shot" />
-              <div>
-                <button
-                  className="btn btn-sm btn-secondary"
-                  onClick={() => {
-                    this.onChangePictureClick(c, index);
-                  }}
-                >
-                  Change Picture
-                </button>
-              </div>
-            </td>
-            <td>{c.name}</td>
-            {/* use of conditional rendering on contact to check if true/ false, display one or the other */}
-            <td>
-              {/* if true display contact else(false, null, undefined) render the div tag */}
-              {this.getContactsToRender(c.contacts)}
-            </td>
-            <td>{c.address.city}</td>
+  return (
+    <>
+      <h4 className="m-2 p-2">
+        Customers
+
+        <span className="badge text-bg-secondary m-2">
+          {customersCount}
+        </span>
+
+        <button className="btn btn-info" onClick={onReloadClick}>
+          Reload
+        </button>
+      </h4>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Picture</th>
+            <th>Customer Name</th>
+            <th>Contact Info</th>
+            <th>City</th>
           </tr>
-        );
-      })
-    );
-  };
-
-  // Fires when user clicks on "Change Picture" button;
-  // Receives the "customers" object and index of the currently clicked customer;
-  onChangePictureClick = (c, index) => {
-    // console.log(c, index);
-
-    
-    // Passing property of 'image' to update through setState with the new url;
-    // Getting the default value/ exisiting customers;
-    let customerList = this.state.customers;
-    customerList[index].image = "https://picsum.photos/id/77/60";
-    // Updating customer images using state hook;
-    this.setState({customers: customerList});
-  };
-}
+        </thead>
+        <tbody>
+          {customers.map((c, index)=> (
+            <tr key={c.id}>
+              <td>{c.id}</td>
+              <td>
+                <img src={c.image} alt="Mug-Shot" />
+                <div>
+                  <button
+                    className="btn btn-sm btn-secondary"
+                    onClick={() => onChangePictureClick(index)}
+                  >
+                    Change Picture
+                  </button>
+                </div>
+              </td>
+              <td>{c.name}</td>
+              <td>{getContactsToRender(c.contacts)}</td>
+              <td>{c.address.city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default CustomersList;

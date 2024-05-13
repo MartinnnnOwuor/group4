f1074ec1603915ef018eed3a5ae2ce23a861967a
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-05-13 17:02:41 +0300

    quality of life update

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Dashboard() {
   return (
+
     <div className='container'>
-      <h5 className='text-center mt-3'>We deal with products of various varieties &#128515;</h5>
+      <form action="">
+        <input type="text" id ="search-input" placeholder="search for images" />
+        <button id="search-button">Search...</button>       
+    </form>
+    
+      <h5 >We deal with products of various varieties &#128515;</h5>
       <div className='row justify-content-center'>
         <div className='col-md-6'>
           <p className='text-center text-primary'>Tablets</p>

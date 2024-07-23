0581f04473b4007ffcaa8a84e054edfc9141775c
+ <+>
2024-07-24 00:56:45 +0300

    feat:add footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className=''>
+      <div className=''>
+        <h2 className=''>E Kart Limited</h2>
+        <p>Connect with us on social media for exciting news, insights and engaging discussions.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer

6348681224e617977c8daa44ad977edd6559802e
+ <+>
2024-09-09 18:49:56 +0300

    refactor:modify contact us page

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Footer from './Footer'
+import React from "react";
+import Footer from "./Footer";
 
 function Contact() {
   return (
     <>
-      <Footer/>
+      <div>
+        <h2>Contact us</h2>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
